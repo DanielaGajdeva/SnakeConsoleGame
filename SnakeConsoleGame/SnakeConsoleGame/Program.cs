9eb88e9c1242fd938bf6f735427ca50cdbf761e3
DanielaGajdeva <daniela.gaideva@gmail.com>
2020-06-21 18:13:48 +0300

    Added project solution folder.

---
+++ SnakeConsoleGame/SnakeConsoleGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
