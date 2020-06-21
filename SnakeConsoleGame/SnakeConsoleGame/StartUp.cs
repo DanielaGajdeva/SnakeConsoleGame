ade2934933475f056dff55dba3036b105facb3df
DanielaGajdeva <daniela.gaideva@gmail.com>
2020-06-21 19:18:49 +0300

    Added main folders and configured console in ConsoleWindow.cs

---
+++ SnakeConsoleGame/SnakeConsoleGame/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeConsoleGame
+{
+    using Utilities;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleWindow.CustomizeConsole();
+        }
+    }
+}
